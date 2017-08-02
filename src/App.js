689b7ba79e95e5885db3f503749b7a64235d8c30
Christiaan Maks <christilut@gmail.com>
2017-08-02 21:55:41 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function createMarkup(html) {
+  return { __html: html }
+}
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      posts: require('./headless/content/blog.json')
+    }
+  }
+
+  render() {
+    const posts = this.state.posts
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div>
+          {
+            posts.map((item, i) => {
+              return <div>
+                <h3>{item.content.en.Title}</h3>
+                <div dangerouslySetInnerHTML={createMarkup(item.content.en.Body)}></div>
+              </div>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
